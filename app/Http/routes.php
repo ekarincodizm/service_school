a13863424fe4646b0dcba3ad941aa925a268fc46
Amera <light.amera@gmail.com>
2017-08-07 01:44:31 +0700

    Revert "เพิ่ม student route"
    This reverts commit ec4d8e278808ed11fb3901adba261046830cb5da.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,6 +4,5 @@ Route::group(['middleware' => ['cors']], function(){
 	
 	Route::controller ( 'user', 'UserController' );
 	Route::controller ( 'subject', 'SubjectController' );
-	Route::controller ( 'student', 'StudentController' );
 			
 });
