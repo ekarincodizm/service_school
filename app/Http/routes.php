c1f08a462018ffb1b1f41acb9c284a33a37b3fbc
Amera <light.amera@gmail.com>
2017-08-07 01:49:05 +0700

    เพิ่ม นักเรียน

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,5 +4,6 @@ Route::group(['middleware' => ['cors']], function(){
 	
 	Route::controller ( 'user', 'UserController' );
 	Route::controller ( 'subject', 'SubjectController' );
+	Route::controller ( 'student', 'StudentController' );
 			
 });
