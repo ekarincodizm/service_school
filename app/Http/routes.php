a6c8628b386eaba0bffc2c9027189f1dc2a67338
Amera <light.amera@gmail.com>
2017-08-24 19:50:23 +0700

    ขาด

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,7 @@ Route::group(['middleware' => ['cors']], function(){
 	Route::controller ( 'user', 'UserController' );
 	Route::controller ( 'subject', 'SubjectController' );
 	Route::controller ( 'student', 'StudentController' );
+	Route::controller ( 'parent', 'ParentController' );
 	Route::controller ( 'room', 'RoomController' );
 	Route::controller ( 'class-room', 'ClassRoomController' );
 	Route::controller ( 'address', 'AddressController' );
