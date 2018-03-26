3e1ae6d3f22fc2b11cefd3945badebed9d98743e
Amera <light.amera@gmail.com>
2018-03-26 22:36:28 +0700

    แก้รูปบัตร

--- resources/views/report/parent-card-v2.blade.php
+++ resources/views/report/parent-card-v2.blade.php
@@ -7,37 +7,37 @@
     		background-repeat: no-repeat;
 			background-size: 100% 100%;">
 	
-	<div style="height:2.13cm;"></div>
+	<div style="height:1.7cm;"></div>
 			   
 	<table style="border-spacing: 0px; width:98.5%;">
 		<tbody>
 			<tr>
-				<td style="width:100%; text-align:center;" valign="middle"><img width="2cm" height="2.65cm" src="{{ $parentPicUrl }}"></td>
+				<td style="width:100%; text-align:center;" valign="middle"><img width="2.83cm" height="3.75cm" src="{{ $parentPicUrl }}"></td>
 			</tr>
 		</tbody>
 	</table>
 	
-	<div style="height:0.35cm;"></div>
+	<div style="height:0.09cm;"></div>
 
 	<table style="border-spacing: 0px; width:98.5%;">
 		<tbody>
 			<tr>
 				<td style="text-align:center;" valign="middle">
-					<p>{{$parent->SP_TITLE_NAME}} {{$parent->SP_FIRST_NAME}} {{$parent->SP_LAST_NAME}}</p>
+					<span style="font-size:16px;">{{$parent->SP_TITLE_NAME}} {{$parent->SP_FIRST_NAME}} {{$parent->SP_LAST_NAME}}</span >
 				</td>
 			</tr>
 		</tbody>
 	</table>
 
-	<div style="height:0.25cm;"></div>
+	<div style="height:0.01cm;"></div>
 
 	<table style="border-spacing: 0px; width:98.5%;">
 		<tbody>
 			<tr>
 				<td style="text-align:center;" valign="middle">
-					<p>ผู้ปกครองของ</p>
+					<span style="font-size:16px;">ผู้รับนักเรียน</span ><br/>
+					<span style="font-size:16px;">{{$student->SA_TITLE_NAME_TH}} {{$student->SA_FIRST_NAME_TH}} {{$student->SA_LAST_NAME_TH}}</span ><br/>
 					<h3>น้อง{{$student->SA_NICK_NAME_TH}}</h3>
-					<p>{{$student->SA_TITLE_NAME_TH}} {{$student->SA_FIRST_NAME_TH}} {{$student->SA_LAST_NAME_TH}}</p>
 				</td>
 			</tr>
 		</tbody>
--- resources/views/report/student-card-v2.blade.php
+++ resources/views/report/student-card-v2.blade.php
@@ -7,24 +7,24 @@
     		background-repeat: no-repeat;
 			background-size: 100% 100%;">
 	
-	<div style="height:2.13cm;"></div>
+	<div style="height:1.98cm;"></div>
 			   
 	<table style="border-spacing: 0px; width:98.5%;">
 		<tbody>
 			<tr>
-				<td style="width:100%; text-align:center;" valign="middle"><img width="2cm" height="2.65cm" src="{{ $studentPicUrl }}"></td>
+				<td style="width:100%; text-align:center;" valign="middle"><img width="3cm" height="3.975cm" src="{{ $studentPicUrl }}"></td>
 			</tr>
 		</tbody>
 	</table>
 	
-	<div style="height:0.5cm;"></div>
+	<div style="height:0.2cm;"></div>
 
 	<table style="border-spacing: 0px; width:98.5%;">
 		<tbody>
 			<tr>
 				<td style="text-align:center;" valign="middle">
-					<h3>น้อง{{$student->SA_NICK_NAME_TH}}</h3>
-					<p>{{$student->SA_TITLE_NAME_TH}} {{$student->SA_FIRST_NAME_TH}} {{$student->SA_LAST_NAME_TH}}</p>
+					<span style="font-size:20px;">{{$student->SA_TITLE_NAME_TH}} {{$student->SA_FIRST_NAME_TH}} {{$student->SA_LAST_NAME_TH}}</span><br/>
+					<h2>น้อง{{$student->SA_NICK_NAME_TH}}</h2>
 				</td>
 			</tr>
 		</tbody>
